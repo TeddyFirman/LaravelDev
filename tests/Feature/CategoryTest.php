32966deb297be01ccf45620d42fdff17ba546f79
TeddyFirman <teddyfirman902@gmail.com>
2023-03-24 15:47:51 +0700

    Update Dan Perbaikan

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -4,11 +4,13 @@ namespace Tests\Feature;
 
 use App\Category as AppCategory;
 use App\Models\Category;
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Response;
 
 class CategoryTest extends TestCase
@@ -18,76 +20,73 @@ class CategoryTest extends TestCase
      *
      * @return void
      */
+    use RefreshDatabase, WithFaker, WithoutMiddleware;
+
+
+
     public function testExample()
     {
         $this->assertTrue(true);
     }
 
-    use RefreshDatabase, WithFaker, WithoutMiddleware;
-
-    public function testGetCategories()
+    public function test_category_listing()
     {
-        // create some categories
-        AppCategory::factory()->count(5)->create();
 
-        // make request to /api/categories
         $response = $this->get('/api/categories');
 
-        // assert response status code is 200
-        $response->assertStatus(Response::HTTP_OK);
-
-        // assert response contains 5 categories
-        $this->assertCount(5, json_decode($response->getContent())->data);
+        $response->assertStatus(200);
     }
 
-    public function testCreateCategory()
+    public function test_category_create()
     {
-        // set category data
-        $categoryData = [
-            'name' => 'Test Category',
-            'is_publish' => true
+        factory(AppCategory::class)->make();
+
+        $response = $this->post('/api/categories/', [
+            'name' => 'apple',
+            'is_publish' => 1
+        ]);
+
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('categories', [
+            'name' => 'apple',
+            'is_publish' => 1
+        ]);
+    }
+
+    public function test_can_update_category()
+    {
+
+        $data = [
+            'name' => 'Jeruk',
+            'is_publish' => 1,
         ];
 
-        // make request to /api/categories with POST method
-        $response = $this->post('/api/categories', $categoryData);
 
-        // assert response status code is 201
-        $response->assertStatus(Response::HTTP_CREATED);
+        $categories = AppCategory::create($data);
 
-        // assert response contains created category
-        $this->assertJsonStringEqualsJsonString(json_encode(['data' => $categoryData]), $response->getContent());
-    }
 
-    public function testUpdateCategory()
-    {
-        // create a category
-        $category = AppCategory::factory()->create();
-
-        // set updated category data
-        $updatedCategoryData = [
-            'name' => 'Updated Test Category',
-            'is_publish' => false
+        $datas = [
+            'name' => 'Es Jeruk',
+            'is_publish' => 0,
         ];
 
-        // make request to /api/categories/{id} with PUT method
-        $response = $this->put("/api/categories/{$category->id}", $updatedCategoryData);
 
-        // assert response status code is 200
-        $response->assertStatus(Response::HTTP_OK);
+        $response = $this->json('PUT', '/api/categories/' . $categories->id, $datas);
 
-        // assert response contains updated category
-        $this->assertJsonStringEqualsJsonString(json_encode(['data' => $updatedCategoryData]), $response->getContent());
+
+        $response->assertStatus(302);
+
+
+        $this->assertDatabaseHas('categories', $data);
     }
 
-    public function testDeleteCategory()
+    public function test_can_delete_category()
     {
-        // create a category
-        $category = AppCategory::factory()->create();
 
-        // make request to /api/categories/{id} with DELETE method
-        $response = $this->delete("/api/categories/{$category->id}");
+        $response = $this->delete('/api/categories/{category}');
 
-        // assert response status code is 204
-        $response->assertStatus(Response::HTTP_NO_CONTENT);
+        $response->assertStatus(302);
     }
 }
